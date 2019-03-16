21d89af74822abb673897f31eb093537989e054c
Johnathan Louie <johnlouie88@outlook.com>
2019-03-15 21:34:53 -0700

    Add data generator class

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import jl
 from keras.models import load_model
 from keras.applications.vgg16 import VGG16
 from sklearn.model_selection import train_test_split
+import keras
 
 
 def categ():
@@ -135,6 +136,21 @@ def resize_imgs(src_list, dst_list):
     return
 
 
+def resize_imgs2(src_list, dst_list):
+    res = (jl.w, jl.h)
+    for src, dst in zip(src_list, dst_list):
+        print(src)
+        print(dst)
+        img = cv2.imread(src, cv2.IMREAD_COLOR)
+        print(img.shape)
+        img2 = cv2.resize(img, res, interpolation=cv2.INTER_CUBIC)
+        print(img2.shape)
+        jl.mkdirs(dst)
+        cv2.imwrite(dst, img2)
+    print('done')
+    return
+
+
 def main():
     """
     Prepare data files, classify, and analyze pipeline.
@@ -173,7 +189,9 @@ def lamem_read(txt):
     for i, v in enumerate(a):
         a[i] = v.split(' ')
     b = np.asarray(a)
-    return b
+    x = np.array(b[:, 0])
+    y = np.array([float(x)*100 for x in b[:, 1]])
+    return x, y
 
 
 def lamem_src_url(url):
@@ -186,23 +204,40 @@ def lamem_dst_url(url):
     return os.path.normpath(a)
 
 
-def lamem_urls(urls, name):
+def lamem_urls(urls):
     src = list(map(lamem_src_url, urls))
     dst = list(map(lamem_dst_url, urls))
     return src, dst
 
 
+def lamem_single_prep(name, n):
+    x, y = lamem_read('%s_%d' % (name, n))
+    src, dst = lamem_urls(x)
+    resize_imgs2(src, dst)
+    print('Converting x to array')
+    dst_array = np.asarray(dst)
+    print('Saving x')
+    jl.npsave('%s%dx' % (name, n), dst_array)
+    print('Converting y to array')
+    y_array = np.asarray(y)
+    print('Saving y')
+    jl.npsave('%s%dy' % (name, n), y_array)
+    print('Finished %s %d' % (name, n))
+    return
+
+
 def lamem_prep():
-    train1 = 'train_1'
-    a = lamem_read(train1)
+    lamem_single_prep('train', 1)
+    lamem_single_prep('val', 1)
+    lamem_single_prep('test', 1)
     # src, dst = lamem_urls(a[:, 0], train1)
     # for i in range(0, 15):
     #     jl.writetxt('gen/%s_%d_src.txt' % (train1, i), src[i*3000:(i+1)*3000])
     #     jl.writetxt('gen/%s_%d_dst.txt' % (train1, i), dst[i*3000:(i+1)*3000])
-    rates = a[:, 1]
-    rates = jl.floatize(rates)
-    for i in range(15):
-        jl.npsave('%s_%d_rates' % (train1, i), rates[i*3000:(i+1)*3000])
+    # rates = a[:, 1]
+    # rates = jl.floatize(rates)
+    # for i in range(15):
+    #     jl.npsave('%s_%d_rates' % (train1, i), rates[i*3000:(i+1)*3000])
     # for i in range(0, 15):
     #     jl.writetxt('gen/%s_%d_src.txt' % (train1, i), src[i*3000:(i+1)*3000])
     # resize_imgs('gen/train1src.txt', 'gen/train1dst.txt')
@@ -249,17 +284,35 @@ def trainrater():
     return
 
 
-model = load_model(jl.H5_RATER, custom_objects={'rmse': rater.rmse})
+class Sequence1(keras.utils.Sequence):
+
+    def __init__(self, x_set, y_set, batch_size):
+        self.x = x_set
+        self.y = y_set
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        a = idx * self.batch_size
+        b = (idx + 1) * self.batch_size
+        batch_x = self.x[a:b]
+        batch_y = self.y[a:b]
+        xx = np.array([cv2.imread(filename) for filename in batch_x])
+        yy = np.array(batch_y)
+        return xx, yy
 
 
 def trainrater2(zxc):
+    model = load_model(jl.H5_RATER, custom_objects={'rmse': rater.rmse})
     x_file = '%s_%d' % ('train_1', zxc)
     y_file = '%s_%d_rates' % ('train_1', zxc)
-    print(zxc)
-    x = jl.npload(x_file)
     print('x')
-    y = jl.npload(y_file)
+    x = jl.npload(x_file)
     print('y')
+    y = jl.npload(y_file)
+    print('z')
     model.fit(
         x,
         y,
@@ -269,6 +322,19 @@ def trainrater2(zxc):
         epochs=1,
         batch_size=15
     )
+    model.save(jl.H5_RATER)
+    return
+
+
+def rater_train3():
+    x = jl.npload('train1x')
+    y = jl.npload('train1y')
+    x1 = jl.npload('val1x')
+    y1 = jl.npload('val1y')
+    model = load_model(jl.H5_RATER, custom_objects={'rmse': rater.rmse})
+    seq1 = Sequence1(x, y, 15)
+    seq2 = Sequence1(x1, y1, 15)
+    model.fit_generator(generator=seq1, epochs=1000, verbose=1, validation_data=seq2, shuffle=False, use_multiprocessing=True)
     model.save(jl.H5_RATER)
     return
 
@@ -319,9 +385,7 @@ def wtfh():
 # lamem_prep()
 # rater_prep()
 # trainrater()
-for j in range(3):
-    for i in range(13):
-        trainrater2(i)
+rater_train3()
 # rater_valid()
 # rater_test()
 # rater_predict()
