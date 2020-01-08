82182b610a812b4d6ed2f19dd4111eefbb7ec794
Johnathan Louie <johnlouie88@outlook.com>
2020-01-08 14:11:42 -0800

    Fixed total epoch when reloading from save

--- archidata.py
+++ archidata.py
@@ -173,7 +173,7 @@ class ArchitectureSplit(object):
         self._lr = dh.get_lr()
         self._mcp = dh.get_mcp()
         self._mcpb = dh.get_mcpb()
-        self._pcp = PickleCheckpoint(self._mcp, self._mcpb, self._lr, self.name())
+        self._pcp = PickleCheckpoint(self._mcp, self._mcpb, self._lr, self.name(), dh.total_epoch)
         return
 
     def _delete(self) -> None:
