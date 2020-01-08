517f654d3988dcf4f4a8775f26e3e100f1131ecb
Johnathan Louie <johnlouie88@outlook.com>
2020-01-07 17:09:49 -0800

    Updated main.py api

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
-from os import environ
-environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import aaa
+from archidata import ArchitectureSet, ArchitectureSplit
+from cc import Ccc, Ccr, CcrCategorical
+from classifier import Vgg16A, Vgg16B, Vgg16C
+from dataset import DataSet
+from lamem import Lamem
+from model import LOSS, METRIC, OPTIMIZER, Architecture, ModelFactory
+from rater import Smi13, Smi13_1, Smi13_2
 
 
-# ccr = Ccr()
-# # ccr.prepare()
-# smi13_1 = Architecture(Smi13(), LOSS[14], OPTIMIZER[0], METRIC[0])
-# ccr1 = ArchitectureSet(smi13_1, ccr)
-# ccr1_1 = ccr1.split(1)
-# # ccr1_1.reset()
-# ccr1_1.train()
+def create(mf: ModelFactory, ds: DataSet, split: int, l: int, o: int, m: int) -> ArchitectureSplit:
+    """
+    Compiles the dataset, architecture, and options into a split.
+    """
+    a = Architecture(mf, LOSS[l], OPTIMIZER[o], METRIC[m])
+    aset = ArchitectureSet(a, ds)
+    return aset.split(split)
 
-# ccc_prep()
-# model.ccc()
-# train('vgg16', 'ccc', 1)
-# test('vgg16', 'ccc', 1)
-# predict('vgg16', 'ccc', 1)
 
-# ccc_prep()
-# model.ccc2()
-# train('vgg16a', 'ccc', 1)
-# test('vgg16a', 'ccc', 1)
-# predict('vgg16a', 'ccc', 1)
+# MODEL FACTORIES
+vgg = Vgg16A()
+vgg1 = Vgg16B()
+vgg2 = Vgg16C()
+smi13 = Smi13()
+smi131 = Smi13_1()
+smi132 = Smi13_2()
 
-# ccc_prep()
-# model.ccc3()
-# train('vgg16b', 'ccc', 1)
-# test('vgg16b', 'ccc', 1)
-# predict('vgg16b', 'ccc', 1)
+# DATASETS
+ccr = Ccr()
+ccc = Ccc()
+ccrc = CcrCategorical()
+lamem = Lamem()
 
-# ccr_prep()
-# model.ccr()
-# train('kcnn', 'ccr', 1, custom=model.custom_rmse)
-# test('kcnn', 'ccr', 1, custom=model.custom_rmse)
-# predict('kcnn', 'ccr', 1, custom=model.custom_rmse)
+# PREPARE DATASETS
+# ccr.prepare()
+# ccc.prepare()
+# ccrc.prepare()
+# lamem.prepare()
 
-# lamem_prep_all()
-# model.lamem()
-# train('kcnn', 'lamem', 1, custom=model.custom_rmse)
-# test('kcnn', 'lamem', 1, custom=model.custom_rmse)
-# predict('kcnn', 'lamem', 1, custom=model.custom_rmse)
+# ARCHITECTURE-DATASET COMBOS
+# archisplit = create(vgg, ccc, 1, 8, 0, 2)
+# archisplit = create(vgg1, ccc, 1, 8, 0, 2)
+# archisplit = create(vgg2, ccc, 1, 8, 0, 2)
+archisplit = create(smi13, ccr, 1, 14, 7, 0)
+# archisplit = create(smi13, lamem, 1, 14, 0, 0)
+# archisplit = create(smi13, lamem, 1, 14, 0, 0)
+# archisplit = create(smi13, ccr, 1, 14, 0, 0)
+
+# RUN
+archisplit.reset()
+archisplit.train()
