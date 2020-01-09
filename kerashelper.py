894f1cbb18487c6ef286fbc3648b42fdd1d962d4
Johnathan Louie <johnlouie88@outlook.com>
2020-01-08 17:56:35 -0800

    Update kerashelper.py

--- kerashelper.py
+++ kerashelper.py
@@ -36,32 +36,7 @@ class Sequence1(Sequence):
 
 class PickleCheckpoint(Callback):
     """
-    Save the model after every epoch.
-    `filepath` can contain named formatting options,
-    which will be filled with the values of `epoch` and
-    keys in `logs` (passed in `on_epoch_end`).
-    For example: if `filepath` is `weights.{epoch:02d}-{val_loss:.2f}.hdf5`,
-    then the model checkpoints will be saved with the epoch number and
-    the validation loss in the filename.
-    # Arguments
-        filepath: string, path to save the model file.
-        monitor: quantity to monitor.
-        verbose: verbosity mode, 0 or 1.
-        save_best_only: if `save_best_only=True`,
-            the latest best model according to
-            the quantity monitored will not be overwritten.
-        save_weights_only: if True, then only the model's weights will be
-            saved (`model.save_weights(filepath)`), else the full model
-            is saved (`model.save(filepath)`).
-        mode: one of {auto, min, max}.
-            If `save_best_only=True`, the decision
-            to overwrite the current save file is made
-            based on either the maximization or the
-            minimization of the monitored quantity. For `val_acc`,
-            this should be `max`, for `val_loss` this should
-            be `min`, etc. In `auto` mode, the direction is
-            automatically inferred from the name of the monitored quantity.
-        period: Interval (number of epochs) between checkpoints.
+    It saves the training state whenever ModelCheckpoint saves the training model.
     """
 
     def __init__(self, mcp: ModelCheckpoint, mcpb: ModelCheckpoint, lr: ReduceLROnPlateau, archisplit: str, total_epoch: int) -> None:
@@ -72,6 +47,7 @@ class PickleCheckpoint(Callback):
         self._lr = lr
         self._total_epoch = total_epoch
         self._archisplit = archisplit
+        return
 
     def on_epoch_end(self, epoch, logs=None) -> None:
         logs = logs or {}
@@ -106,6 +82,7 @@ class PickleCheckpoint(Callback):
                     dh.save()
                 else:
                     dh.save()
+        return
 
     def _copy_mcp(self, mcp: ModelCheckpoint) -> None:
         """
