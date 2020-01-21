51de38ac004c0f0d31fca17bd73100d64c2b223f
Johnathan Louie <johnlouie88@outlook.com>
2020-01-20 23:58:20 -0800

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,11 @@
-import jl
-import numpy as np
+from numpy import argmax, zeros
+
+from cc import CcDataFile
+from jl import NPY_CLASSES, NPY_PRED, TEXT_PRED, ListFile, npload
 
 
 def onenumber(onehot):
-    p1 = np.argmax(onehot, 1)
+    p1 = argmax(onehot, 1)
     return p1
 
 
@@ -18,12 +20,12 @@ def main():
     # col = jl.getcol(2)
     # truth = jl.numberize(col)
     # pred = jl.readtxt('cpred.txt')
-    pred = jl.npload(jl.NPY_PRED)
-    truth = jl.npload(jl.NPY_CLASSES)
+    pred = npload(NPY_PRED)
+    truth = npload(NPY_CLASSES)
     pred = onenumber(pred)
     truth = onenumber(truth)
-    jl.writetxt(jl.TEXT_PRED, oneup(pred))
-    cm = np.zeros((7, 7))
+    ListFile(TEXT_PRED).write(oneup(pred))
+    cm = zeros((7, 7))
     for t, p in zip(truth, pred):
         p1 = p+1
         t1 = t+1
@@ -32,9 +34,10 @@ def main():
     label = "%11s %4s %3s%% | %4s %4s %4s %4s %4s %4s" % (
         '', '', '', 'env', 'peop', 'obj', 'hybr', 'anim', 'food')
     print(label)
+    ccdf = CcDataFile()
     for i in range(1, 7):
         asd = "%11s %4d %3d%% | %4d %4d %4d %4d %4d %4d" % (
-            jl.classdict2[i], cm[i][0], cm[i][i]/cm[i][0]*100, cm[i][1], cm[i][2], cm[i][3], cm[i][4], cm[i][5], cm[i][6])
+            ccdf._to_category_str(i - 1), cm[i][0], cm[i][i]/cm[i][0]*100, cm[i][1], cm[i][2], cm[i][3], cm[i][4], cm[i][5], cm[i][6])
         print(asd)
     return
 
