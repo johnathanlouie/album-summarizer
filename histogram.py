29e4776c6dd2a2cdb6fc460f397210839f518b9a
Johnathan Louie <johnlouie88@outlook.com>
2020-01-21 00:39:50 -0800

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,13 +1,14 @@
-import jl
-import time
-import cv2 as cv
-import numpy as np
+from numpy import concatenate, reshape, vstack
 from sklearn.cluster import MeanShift
+
+import cv2 as cv
+from jl import (TEXT_CLUSTER_HISTOGRAM, TEXT_URL_ALBUM, ListFile, Stopwatch,
+                hsvlist, readimg2)
 
 
 def calchist(img, channel, val):
     a = cv.calcHist([img], [channel], None, [val], [0, val])
-    b = np.reshape(a, (a.size))
+    b = reshape(a, (a.size))
     return b
 
 
@@ -23,23 +24,23 @@ def cluster(images, bandwidth):
         c1 = norm(calchist(i, 0, 180), res)
         c2 = norm(calchist(i, 1, 256), res)
         c3 = norm(calchist(i, 2, 256), res)
-        c4 = np.concatenate((c1, c2, c3))
+        c4 = concatenate((c1, c2, c3))
         c.append(c4)
         # c.append(c1)
-    d = np.vstack(c)
+    d = vstack(c)
     f = MeanShift(bandwidth).fit(d)
     return f.labels_
 
 
 def main():
-    sw = jl.Stopwatch()
+    sw = Stopwatch()
     print('reading urls')
-    a = jl.readtxt(jl.TEXT_URL_ALBUM)
+    a = ListFile(TEXT_URL_ALBUM).read()
     print('reading images')
-    b = jl.readimg2(a)
-    b = jl.hsvlist(b)
+    b = readimg2(a)
+    b = hsvlist(b)
     g = cluster(b, .09)
-    jl.writetxt(jl.TEXT_CLUSTER_HISTOGRAM, g)
+    ListFile(TEXT_CLUSTER_HISTOGRAM).write(g)
     sw.print()
     return
 
