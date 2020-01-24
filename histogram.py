c4b7c9bef2702483bfeb99015167d573c1c89666
Johnathan Louie <johnlouie88@outlook.com>
2020-01-24 14:47:20 -0800

    Update style

--- histogram.py
+++ histogram.py
@@ -1,47 +1,63 @@
-from numpy import concatenate, reshape, vstack
+from typing import List
+
+from numpy import concatenate, ndarray, reshape, vstack
 from sklearn.cluster import MeanShift
 
-import cv2 as cv
-from jl import (TEXT_CLUSTER_HISTOGRAM, TEXT_URL_ALBUM, ListFile, Stopwatch,
-                hsvlist, readimg2)
+import cv2
+from jl import (TEXT_CLUSTER_HISTOGRAM, Image, ImageDirectory, ListFile,
+                Stopwatch, Url, hsvlist, readimg2)
 
 
-def calchist(img, channel, val):
-    a = cv.calcHist([img], [channel], None, [val], [0, val])
-    b = reshape(a, (a.size))
-    return b
+def calchist(img: Image, channel: int, range: int) -> ndarray:
+    """
+    """
+    histogram = cv2.calcHist(images=[img], channels=[channel], mask=None, histSize=[range], ranges=[0, range])
+    histogram2 = reshape(histogram, (histogram.size))
+    return histogram2
 
 
-def norm(histogram, resolution):
-    return histogram / resolution
+def scale_histogram(histogram: ndarray, height: int, width: int) -> ndarray:
+    """
+    """
+    pixels = height * width
+    return histogram / pixels
 
 
-def cluster(images, bandwidth):
+def cluster(images: List[Image], bandwidth: float) -> List[int]:
+    """
+    """
     c = list()
     for i in images:
         h, w, _ = i.shape
-        res = h * w
-        c1 = norm(calchist(i, 0, 180), res)
-        c2 = norm(calchist(i, 1, 256), res)
-        c3 = norm(calchist(i, 2, 256), res)
+        x1 = calchist(i, 0, 180)
+        x2 = calchist(i, 1, 256)
+        x3 = calchist(i, 2, 256)
+        c1 = scale_histogram(x1, h, w)
+        c2 = scale_histogram(x2, h, w)
+        c3 = scale_histogram(x3, h, w)
         c4 = concatenate((c1, c2, c3))
         c.append(c4)
         # c.append(c1)
     d = vstack(c)
-    f = MeanShift(bandwidth).fit(d)
-    return f.labels_
+    ms = MeanShift(bandwidth)
+    ms.fit(d)
+    results = ms.labels_.tolist()
+    return results
 
 
-def main():
-    sw = Stopwatch()
-    print('reading urls')
-    a = ListFile(TEXT_URL_ALBUM).read()
-    print('reading images')
-    b = readimg2(a)
-    b = hsvlist(b)
-    g = cluster(b, .09)
-    ListFile(TEXT_CLUSTER_HISTOGRAM).write(g)
-    sw.print()
+def main(directory: Url) -> None:
+    """
+    """
+    print('Reading image directory....')
+    image_urls = ImageDirectory(directory).jpeg()
+    print('Loading images....')
+    images = readimg2(image_urls)
+    print('Converting images to hue-saturation-value representation....')
+    images2 = hsvlist(images)
+    print('Clustering....')
+    c = cluster(images2, .09)
+    print('Saving....')
+    ListFile(TEXT_CLUSTER_HISTOGRAM).write(c)
     return
 
 
