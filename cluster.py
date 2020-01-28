95bb38a04195b40d10d958d9cdea9ffd24b62a5c
Johnathan Louie <johnlouie88@outlook.com>
2020-01-28 00:54:29 -0800

    Create cluster.py

---
+++ cluster.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from jl import Url
+
+
+class ImageCluster(object):
+    """
+    """
+
+    def run(self, directory: Url) -> List[int]:
+        """
+        """
+        raise NotImplementedError
