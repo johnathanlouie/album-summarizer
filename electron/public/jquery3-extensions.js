2817054667765eeeef98a34f18f479ce22eb1d29
Johnathan Louie <johnathanlouie@outlook.com>
2021-01-19 18:57:49 -0800

    Add readText

--- electron/public/jquery3-extensions.js
+++ electron/public/jquery3-extensions.js
@@ -5,5 +5,18 @@ jQuery.fn.extend({
         } else {
             return this.val(value);
         }
+    },
+    readText() {
+        const fileList = this.prop('files');
+        const promises = [];
+        for (let file of fileList) {
+            promises.push(new Promise((resolve, reject) => {
+                let reader = new FileReader();
+                reader.onload = (event) => resolve(event.target.result);
+                reader.onerror = reject;
+                reader.readAsText(file);
+            }));
+        }
+        return Promise.all(promises);
     }
 });
