b5f6b5ddcfb65867b5ace233210e754ae223d982
Johnathan Louie <johnathanlouie@outlook.com>
2021-01-31 14:00:23 -0800

    Refactor PickleCheckpoint to observer pattern

--- source/core/kerashelper.py
+++ source/core/kerashelper.py
@@ -1,6 +1,11 @@
-from warnings import warn
+from __future__ import annotations
 
+import warnings
+from typing import Dict, List, Union
+
+import dill
 import keras
+import numpy as np
 from jl import Url, resize_img
 from keras.backend import get_value
 from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau
@@ -34,9 +39,10 @@ class Sequence1(Sequence):
         yy = asarray(batch_y)
         return xx, yy
 
+
 class CheckpointObserver(object):
     """
-    Observer interface for ModelCheckpoint2's events.
+    Observer interface for ModelCheckpoint2.
     """
 
     def __init__(self):
@@ -58,90 +64,124 @@ class SaveKmodelObserver(CheckpointObserver):
         kmodel.save_weights(self._url, overwrite=True)
 
 
-class PickleCheckpoint(Callback):
+class ModelCheckpoint2Pickle(object):
     """
-    It saves the training state whenever ModelCheckpoint saves the training model.
     """
 
-    def __init__(self, save_location: Url, mcp: ModelCheckpoint, mcpb: ModelCheckpoint, lr: ReduceLROnPlateau) -> None:
-        super(PickleCheckpoint, self).__init__()
-        self._init(mcp)
-        self._mcp = mcp
-        self._mcpb = mcpb
-        self._lr = lr
-        self._url = save_location
+    def __init__(self, mcp: ModelCheckpoint2) -> None:
+        self._copyTo(mcp, self)
+        self.monitor_op = mcp.monitor_op.__name__
 
-    def on_epoch_end(self, epoch, logs=None) -> None:
-        logs = logs or {}
+    @staticmethod
+    def _copyTo(
+        src: Union[ModelCheckpoint2, ModelCheckpoint2Pickle],
+        dst: Union[ModelCheckpoint2, ModelCheckpoint2Pickle]
+    ) -> None:
+        """
+        Copies the data from one type to another.
+        """
+        dst.best = src.best
+        dst.epochs_since_last_save = src.epochs_since_last_save
+        dst.monitor = src.monitor
+        dst.period = src.period
+
+    def get(self) -> ModelCheckpoint2:
+        """
+        Gets a copy of ModelCheckpoint based on this instance data.
+        """
+        mcp = ModelCheckpoint2('', '')
+        self._copyTo(self, mcp)
+        if self.monitor_op == 'greater':
+            mcp.monitor_op = np.greater
+        elif self.monitor_op == 'less':
+            mcp.monitor_op = np.less
+        else:
+            raise Exception('monitor_op field missing')
+        return mcp
+
+    def save(self, save_location: Url) -> None:
+        dill.dump(self, open(save_location, "wb"))
+
+    @staticmethod
+    def load(save_location: Url) -> ModelCheckpoint2Pickle:
+        return dill.load(open(save_location, 'rb'))
+
+
+class ModelCheckpoint2(Callback):
+    """
+    Subject for CheckpointObserver.
+    """
+
+    def __init__(
+        self,
+        save: Url,
+        save_best: Url,
+        mode: str = 'auto',
+        period: int = 1,
+    ):
+        super(ModelCheckpoint2, self).__init__()
+        self.url: Url = save
+        self.url2: Url = save_best
+        self.monitor: str = 'val_loss'
+        self.period: int = period
+        self.epochs_since_last_save: int = 0
+        self._periodic: List[CheckpointObserver]
+        self._best: List[CheckpointObserver]
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('ModelCheckpoint2 mode %s is unknown, fallback to auto mode.' % (mode), RuntimeWarning)
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
+
+    def on_epoch_end(self, epoch: int, logs: Dict = None):
+        # Period check
         self.epochs_since_last_save += 1
-        dh = DataHolder(epoch + 1, self._lr, self._mcp, self._mcpb)
         if self.epochs_since_last_save >= self.period:
             self.epochs_since_last_save = 0
-            # filepath = self.filepath.format(epoch=epoch + 1, **logs)
-            if self.save_best_only:
-                current = logs.get(self.monitor)
-                if current is None:
-                    warn('Can save best Keras callback objects only with %s available, skipping.' % (self.monitor), RuntimeWarning)
-                else:
-                    if self.monitor_op(current, self.best):
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f, saving training state to %s' % (epoch + 1, self.monitor, self.best, current, self._url))
-                        self.best = current
-                        if self.save_weights_only:
-                            dh.save(self._url)
-                        else:
-                            dh.save(self._url)
-                    else:
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: %s did not improve from %0.5f' % (epoch + 1, self.monitor, self.best))
+            print('\nEpoch %05d: saving model' % (epoch + 1))
+            self.save(self.url)
+            for observer in self._periodic:
+                observer.callback(self.model, epoch)
+        # Checks if last epoch improved the model.
+        logs = logs or {}
+        current = logs.get(self.monitor)
+        if current is None:
+            warnings.warn('Can save best model only with %s available, skipping.' % (self.monitor), RuntimeWarning)
+        else:
+            if self.monitor_op(current, self.best):
+                print('\nEpoch %05d: %s improved from %0.5f to %0.5f, saving model' % (epoch + 1, self.monitor, self.best, current))
+                self.best = current
+                self.save(self.url2)
+                for observer in self._best:
+                    observer.callback(self.model, epoch)
             else:
-                if self.verbose > 0:
-                    print('\nEpoch %05d: saving training state to %s' % (epoch + 1, self._url))
-                if self.save_weights_only:
-                    dh.save(self._url)
-                else:
-                    dh.save(self._url)
+                print('\nEpoch %05d: %s did not improve from %0.5f' % (epoch + 1, self.monitor, self.best))
 
-    def _init(self, mcp: ModelCheckpoint) -> None:
-        """
-        Initializes this instance by copying a ModelCheckpoint instance.
-        """
-        try:
-            self.best = mcp.best
-        except AttributeError:
-            pass
-        try:
-            self.epochs_since_last_save = mcp.epochs_since_last_save
-        except AttributeError:
-            pass
-        try:
-            self.filepath = mcp.filepath
-        except AttributeError:
-            pass
-        try:
-            self.monitor = mcp.monitor
-        except AttributeError:
-            pass
-        try:
-            self.period = mcp.period
-        except AttributeError:
-            pass
-        try:
-            self.save_best_only = mcp.save_best_only
-        except AttributeError:
-            pass
-        try:
-            self.save_weights_only = mcp.save_weights_only
-        except AttributeError:
-            pass
-        try:
-            self.verbose = mcp.verbose
-        except AttributeError:
-            pass
-        try:
-            self.monitor_op = mcp.monitor_op
-        except AttributeError:
-            pass
+    def save(self, save_location: Url) -> None:
+        ModelCheckpoint2Pickle(self).save(save_location)
+
+    @staticmethod
+    def load(save_location: Url) -> ModelCheckpoint2:
+        return ModelCheckpoint2Pickle.load(save_location).get()
+
+    def set_periodic_observers(self, obs: List[CheckpointObserver]) -> None:
+        self._periodic = obs
+
+    def set_improvement_observers(self, obs: List[CheckpointObserver]) -> None:
+        self._best = obs
 
 
 class TerminateOnDemand(Callback):
