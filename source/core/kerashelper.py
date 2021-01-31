af28cf5936ff9232e3ef07e05afba2284207c938
Johnathan Louie <johnathanlouie@outlook.com>
2021-01-31 15:12:05 -0800

    Delete core.dataholder and move ReduceLROnPlateauPickle

--- source/core/kerashelper.py
+++ source/core/kerashelper.py
@@ -8,11 +8,9 @@ import keras
 import numpy as np
 from jl import Url, resize_img
 from keras.backend import get_value
-from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import Callback, ReduceLROnPlateau
 from keras.utils import Sequence
 from numpy import asarray, ceil
-
-from core.dataholder import DataHolder
 
 
 class Sequence1(Sequence):
@@ -56,6 +54,43 @@ class PickleAbstractClass(object):
     @staticmethod
     def load(save_location: Url) -> PickleAbstractClass:
         return dill.load(open(save_location, 'rb'))
+
+
+class ReduceLROnPlateauPickle(PickleAbstractClass):
+    """
+    A picklable class that contains the data fields from ReduceLROnPlateau.
+    """
+
+    def __init__(self, lr: ReduceLROnPlateau) -> None:
+        self._copyTo(lr, self)
+
+    @staticmethod
+    def _copyTo(
+        src: Union[ReduceLROnPlateauPickle, ReduceLROnPlateau], 
+        dst: Union[ReduceLROnPlateauPickle, ReduceLROnPlateau],
+        ) -> None:
+        """
+        Copies the data from one type to another.
+        """
+        dst.best = src.best
+        dst.cooldown = src.cooldown
+        dst.cooldown_counter = src.cooldown_counter
+        dst.factor = src.factor
+        dst.min_delta = src.min_delta
+        dst.min_lr = src.min_lr
+        dst.mode = src.mode
+        dst.monitor = src.monitor
+        dst.patience = src.patience
+        dst.verbose = src.verbose
+        dst.wait = src.wait
+
+    def get(self) -> ReduceLROnPlateau:
+        """
+        Gets a copy of ReduceLROnPlateau based on this instance data.
+        """
+        other = ReduceLROnPlateau()
+        self._copyTo(self, other)
+        return other
 
 
 class CheckpointObserver(object):
@@ -105,7 +140,7 @@ class ModelCheckpoint2Pickle(object):
 
     def get(self) -> ModelCheckpoint2:
         """
-        Gets a copy of ModelCheckpoint based on this instance data.
+        Gets a copy of ModelCheckpoint2 based on this instance data.
         """
         mcp = ModelCheckpoint2('', '')
         self._copyTo(self, mcp)
