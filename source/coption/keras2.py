4f420a42069e1c8c2b84cd21b7af7b3fb2b83f68
Johnathan Louie <johnathanlouie@outlook.com>
2021-02-05 22:46:00 -0800

    Fix bug

--- source/coption/keras2.py
+++ source/coption/keras2.py
@@ -4,4 +4,4 @@ from core.modelbuilder import ModelBuilder
 from keras.optimizers import SGD
 
 
-ModelBuilder.loss(CompileOption('sgd1', SGD(lr=0.01, momentum=0.9, decay=0.0005, nesterov=True)))
+ModelBuilder.optimizer(CompileOption('sgd1', SGD(lr=0.01, momentum=0.9, decay=0.0005, nesterov=True)))
