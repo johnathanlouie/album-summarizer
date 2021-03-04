de20012d761542ef6cd9e287860dbeb68530e0b9
Johnathan Louie <johnathanlouie@outlook.com>
2021-03-03 17:13:04 -0800

    Move the for loops to a generator and make metrics the outermost for loop

--- source/trainall.py
+++ source/trainall.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import aaa
 import architecture.smi13
 import architecture.vgg16
@@ -10,32 +12,37 @@ import dataset.cc
 import dataset.lamem
 
 
+def builds() -> Tuple[str, str, str, str, str]:
+    for metric in core.modelbuilder.ModelBuilder.METRICS.keys():
+        for architecture in core.modelbuilder.ModelBuilder.ARCHITECTURES.keys():
+            for dataset in core.modelbuilder.ModelBuilder.DATASETS.keys():
+                for loss in core.modelbuilder.ModelBuilder.LOSSES.keys():
+                    for optimizer in core.modelbuilder.ModelBuilder.OPTIMIZERS.keys():
+                        yield architecture, dataset, loss, optimizer, metric
+
+
 def main():
-    for architecture in core.modelbuilder.ModelBuilder.ARCHITECTURES.keys():
-        for dataset in core.modelbuilder.ModelBuilder.DATASETS.keys():
-            for loss in core.modelbuilder.ModelBuilder.LOSSES.keys():
-                for optimizer in core.modelbuilder.ModelBuilder.OPTIMIZERS.keys():
-                    for metric in core.modelbuilder.ModelBuilder.METRICS.keys():
-                        try:
-                            model = core.modelbuilder.ModelBuilder.create(
-                                architecture,
-                                dataset,
-                                loss,
-                                optimizer,
-                                metric,
-                                epochs=0,
-                                patience=3,
-                            )
-                            if not model.is_complete():
-                                model.train()
-                        except ValueError:
-                            print('Incompatible model: %s %s %s %s %s' % (
-                                architecture,
-                                dataset,
-                                loss,
-                                optimizer,
-                                metric,
-                            ))
+    for architecture, dataset, loss, optimizer, metric in builds():
+        try:
+            model = core.modelbuilder.ModelBuilder.create(
+                architecture,
+                dataset,
+                loss,
+                optimizer,
+                metric,
+                epochs=0,
+                patience=3,
+            )
+            if not model.is_complete():
+                model.train()
+        except ValueError:
+            print('Incompatible model: %s %s %s %s %s' % (
+                architecture,
+                dataset,
+                loss,
+                optimizer,
+                metric,
+            ))
 
 
 if __name__ == '__main__':
