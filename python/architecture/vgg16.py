8453781cb12d690efa66907364da0e41dc52ba56
Johnathan Louie <johnathanlouie@outlook.com>
2021-03-16 19:03:23 -0700

    Move deprecated vgg16 info to vgg16.py

--- python/architecture/vgg16.py
+++ python/architecture/vgg16.py
@@ -35,6 +35,22 @@ class Vgg16B(Architecture):
     NAME = 'vgg16b'
     OUTPUT_NUM: int = 6
 
+    LAYERS = [
+        'block1_conv1',
+        'block1_conv2',
+        'block2_conv1',
+        'block2_conv2',
+        'block3_conv1',
+        'block3_conv2',
+        'block3_conv3',
+        'block4_conv1',
+        'block4_conv2',
+        'block4_conv3',
+        'block5_conv1',
+        'block5_conv2',
+        'block5_conv3'
+    ]
+
     def create(self, res: Resolution) -> Model:
         """
         Creates a keras.models.Model object.
--- python/jl.py
+++ python/jl.py
@@ -37,23 +37,6 @@ NPY_RATE = 'rate'
 JSON_SIMILARITYMATRIX = 'out/sim.json'
 
 
-layers = [
-    'block1_conv1',
-    'block1_conv2',
-    'block2_conv1',
-    'block2_conv2',
-    'block3_conv1',
-    'block3_conv2',
-    'block3_conv3',
-    'block4_conv1',
-    'block4_conv2',
-    'block4_conv3',
-    'block5_conv1',
-    'block5_conv2',
-    'block5_conv3'
-]
-
-
 def npsave(name: str, data: ArrayLike, verbose: bool = True) -> None:
     """
     Saves to a binary NumPy file.
