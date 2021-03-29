fc652d3a15f69097936dfeece6a3fafa9773f30a
Johnathan Louie <johnathanlouie@outlook.com>
2021-03-28 21:37:16 -0700

    Change history from object to class

--- electron/public/services/history/history.service.js
+++ electron/public/services/history/history.service.js
@@ -1,34 +1,35 @@
 'use strict';
 
-const history = {
-  _history: [],
-  _future: [],
-  _current: undefined,
-  get hasBack() { return this._history.length === 0; },
-  get hasNext() { return this._future.length === 0; },
-  get current() { return this._current; },
+class History {
+    #history = [];
+    #future = [];
+    #current = undefined;
 
-  push(dir) {
-    dir = path.normalize(dir);
-    if (this._current !== dir) {
-      this._future = [];
-      if (this._current !== undefined) { this._history.push(this._current); }
-      this._current = dir;
+    get hasBack() { return this.#history.length === 0; }
+    get hasNext() { return this.#future.length === 0; }
+    get current() { return this.#current; }
+
+    push(dir) {
+        dir = path.normalize(dir);
+        if (this.#current !== dir) {
+            this.#future = [];
+            if (this.#current !== undefined) { this.#history.push(this.#current); }
+            this.#current = dir;
+        }
+        return this.#current;
     }
-    return this._current;
-  },
 
-  goBack() {
-    this._future.push(this._current);
-    this._current = this._history.pop();
-    return this._current;
-  },
+    goBack() {
+        this.#future.push(this.#current);
+        this.#current = this.#history.pop();
+        return this.#current;
+    }
 
-  goForward() {
-    this._history.push(this._current);
-    this._current = this._future.pop();
-    return this._current;
-  }
-};
+    goForward() {
+        this.#history.push(this.#current);
+        this.#current = this.#future.pop();
+        return this.#current;
+    }
+}
 
-angular.module('services').factory('History', () => history);
+angular.module('services').factory('History', History);
