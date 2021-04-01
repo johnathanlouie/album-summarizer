1396cfdf44725d778d57144f4221906f78239620
Johnathan Louie <johnathanlouie@outlook.com>
2021-03-31 18:12:20 -0700

    Update file-url.filter.js

--- electron/public/core/file-url/file-url.filter.js
+++ electron/public/core/file-url/file-url.filter.js
@@ -1,6 +1,10 @@
 angular.module('core').filter('fileUrl', function () {
     const fileUrl = require('file-url');
     return function (url) {
+        if (typeof url !== 'string') {
+            console.error('fileUrl: Not string', url);
+            return '';
+        }
         return fileUrl(url);
     };
 });
