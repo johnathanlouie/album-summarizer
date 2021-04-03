c50c64af24e9d5f95a7578901a3437ee8ed1d1ff
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-02 21:46:31 -0700

    Update app.config.js

--- electron/public/app.config.js
+++ electron/public/app.config.js
@@ -3,8 +3,9 @@
 
 angular.module('app').config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
+        when('/', { template: '<organizer-view></organizer-view>' }).
         when('/organizer', { template: '<organizer-view></organizer-view>' }).
         when('/menu', { template: '<menu-view></menu-view>' }).
         when('/devtools/preview', { template: '<preview-view></preview-view>' }).
-        otherwise('/organizer');
+        otherwise('/menu');
 }]);
