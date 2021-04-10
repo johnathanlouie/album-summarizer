fdbd4b84c1744866f0710de412a6f7f7eb267223
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-10 01:56:23 -0700

    Add notification after successful save

--- electron/public/services/mongodb.service.js
+++ electron/public/services/mongodb.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-angular.module('services').factory('MongoDB', ['$rootScope', function ($rootScope) {
+angular.module('services').factory('MongoDB', [function () {
     const fs = require('fs');
 
     class Settings {
@@ -21,24 +21,18 @@ angular.module('services').factory('MongoDB', ['$rootScope', function ($rootScop
         }
 
         async save() {
-            try {
-                await fs.promises.writeFile('mongodb.json', JSON.stringify(this));
-                this.#loaded = true;
-            }
-            catch (e) { $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Cannot write to MongoDB settings file.', 'Error: MongoDB'); }
+            await fs.promises.writeFile('mongodb.json', JSON.stringify(this));
+            this.#loaded = true;
         }
 
         async load() {
-            try {
-                this.#loaded = false;
-                var json = await fs.promises.readFile('mongodb.json');
-                var obj = JSON.parse(json);
-                this.hostname = obj.hostname;
-                this.username = obj.username;
-                this.password = obj.password;
-                this.#loaded = true;
-            }
-            catch (e) { $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error while loading MongoDB settings file.', 'Error: MongoDB'); }
+            this.#loaded = false;
+            var json = await fs.promises.readFile('mongodb.json');
+            var obj = JSON.parse(json);
+            this.hostname = obj.hostname;
+            this.username = obj.username;
+            this.password = obj.password;
+            this.#loaded = true;
         }
 
         get uri() {
--- electron/public/views/settings-view/settings-view.component.js
+++ electron/public/views/settings-view/settings-view.component.js
@@ -3,15 +3,26 @@
 
 angular.module('views.settingsView').component('settingsView', {
     templateUrl: 'views/settings-view/settings-view.template.html',
-    controller: ['$scope', 'MongoDB', function ($scope, MongoDB) {
+    controller: ['$scope', '$rootScope', 'MongoDB', function ($scope, $rootScope, MongoDB) {
 
         async function getSettings() {
-            $scope.settings = await MongoDB.settings();
+            try {
+                $scope.settings = await MongoDB.settings();
+            }
+            catch (e) { $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error while loading MongoDB settings file.', 'Error: MongoDB'); }
             $scope.$apply();
         }
 
-        $scope.update = async function () { await $scope.settings.save(); }
+        $scope.update = async function () {
+            try {
+                await $scope.settings.save();
+                $('#toast1').toast('show');
+                $scope.$apply();
+            }
+            catch (e) { $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Cannot write to MongoDB settings file.', 'Error: MongoDB'); }
+        }
 
         getSettings();
+
     }],
 });
