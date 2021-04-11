56f198d866acb1b679d8d91c7ad36b8930e9fa38
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-11 16:22:33 -0700

    Update app.config.js

--- electron/public/app.config.js
+++ electron/public/app.config.js
@@ -2,15 +2,14 @@ const angular = require('angular');
 
 
 angular.module('app').config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-        when('/', { template: '<organizer-view></organizer-view>' }).
-        when('/organizer', { template: '<organizer-view></organizer-view>' }).
-        when('/menu', { template: '<menu-view></menu-view>' }).
-        when('/devtools/check-rate', { template: '<check-rate-view></check-rate-view>' }).
-        when('/devtools/histogram', { template: '<histogram-view></histogram-view>' }).
-        when('/devtools/label-data', { template: '<label-data-view></label-data-view>' }).
-        when('/devtools/preview', { template: '<preview-view></preview-view>' }).
-        when('/devtools/sift-similarity', { template: '<sift-similarity-view></sift-similarity-view>' }).
-        when('/settings', { template: '<settings-view></settings-view>' }).
-        otherwise('/menu');
+    $routeProvider.when('/', { template: '<organizer-view></organizer-view>' });
+    $routeProvider.when('/organizer', { template: '<organizer-view></organizer-view>' });
+    $routeProvider.when('/menu', { template: '<menu-view></menu-view>' });
+    $routeProvider.when('/devtools/check-rate', { template: '<check-rate-view></check-rate-view>' });
+    $routeProvider.when('/devtools/histogram', { template: '<histogram-view></histogram-view>' });
+    $routeProvider.when('/devtools/label-data', { template: '<label-data-view></label-data-view>' });
+    $routeProvider.when('/devtools/preview', { template: '<preview-view></preview-view>' });
+    $routeProvider.when('/devtools/sift-similarity', { template: '<sift-similarity-view></sift-similarity-view>' });
+    $routeProvider.when('/settings', { template: '<settings-view></settings-view>' });
+    $routeProvider.otherwise('/menu');
 }]);
