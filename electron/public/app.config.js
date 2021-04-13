a7b0772d6166957be9ada91b8caa890d93cb88ce
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-12 19:36:44 -0700

    Update app.config.js

--- electron/public/app.config.js
+++ electron/public/app.config.js
@@ -1,13 +1,11 @@
 function configFn($routeProvider) {
     $routeProvider.when('/', { template: '<organizer-view></organizer-view>' });
-    $routeProvider.when('/organizer', { template: '<organizer-view></organizer-view>' });
     $routeProvider.when('/menu', { template: '<menu-view></menu-view>' });
+    $routeProvider.when('/organizer', { template: '<organizer-view></organizer-view>' });
+    $routeProvider.when('/settings', { template: '<settings-view></settings-view>' });
     $routeProvider.when('/devtools/check-rate', { template: '<check-rate-view></check-rate-view>' });
     $routeProvider.when('/devtools/histogram', { template: '<histogram-view></histogram-view>' });
     $routeProvider.when('/devtools/label-data', { template: '<label-data-view></label-data-view>' });
-    $routeProvider.when('/devtools/preview', { template: '<preview-view></preview-view>' });
-    $routeProvider.when('/devtools/sift-similarity', { template: '<sift-similarity-view></sift-similarity-view>' });
-    $routeProvider.when('/settings', { template: '<settings-view></settings-view>' });
     $routeProvider.otherwise('/menu');
 }
 
