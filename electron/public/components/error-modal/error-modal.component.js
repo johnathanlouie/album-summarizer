5eaa0ccc8ea0b5d1a6121a5dc16b7b9944e14480
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-15 01:11:02 -0700

    Update error-modal.component.js

--- electron/public/components/error-modal/error-modal.component.js
+++ electron/public/components/error-modal/error-modal.component.js
@@ -1,9 +1,9 @@
-import controllerFn from './error-modal.controller.js';
+import Controller from './error-modal.controller.js';
 
 
 const componentDef = {
     templateUrl: 'components/error-modal/error-modal.template.html',
-    controller: controllerFn,
+    controller: Controller,
 };
 
 
