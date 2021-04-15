0713542b6da2e9cbbcafe4755c4a4ab871605e68
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-15 01:02:52 -0700

    Update error-modal.controller.js

--- electron/public/components/error-modal/error-modal.controller.js
+++ electron/public/components/error-modal/error-modal.controller.js
@@ -1,13 +1,23 @@
-function controller($scope) {
-    $scope.$on('ERROR_MODAL_SHOW', function (event, error, title, message) {
-        $scope.error = error;
-        $scope.message = message;
-        $scope.title = title;
-        $('#errorModal').modal();
-    });
+const angular = require('angular');
+
+
+class Controller {
+
+    static $inject = ['$scope'];
+
+    /**
+     * @param {angular.IScope} $scope
+     */
+    constructor($scope) {
+        $scope.$on('ERROR_MODAL_SHOW', (event, error, title, message) => {
+            $scope.error = error;
+            $scope.message = message;
+            $scope.title = title;
+            $('#errorModal').modal();
+        });
+    }
+
 }
 
-controller.$inject = ['$scope'];
 
-
-export default controller;
+export default Controller;
