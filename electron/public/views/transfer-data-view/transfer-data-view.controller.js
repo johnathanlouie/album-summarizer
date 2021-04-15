d2beb961fc9dce055d7e84466d6e829db6aeac6e
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-14 19:43:53 -0700

    Update transfer-data-view.controller.js

--- electron/public/views/transfer-data-view/transfer-data-view.controller.js
+++ electron/public/views/transfer-data-view/transfer-data-view.controller.js
@@ -4,15 +4,46 @@ const process = require('process');
 const path = require('path');
 const parseCsv = require('csv-parse/lib/sync');
 const stringifyCsv = require('csv-stringify/lib/sync');
+const angular = require('angular');
+import MongoDbService from '../../services/mongodb.service.js';
 
 
-function controllerFn($scope, $rootScope, mongoDb) {
+class Controller {
 
-    $scope.load = function () {
+    /** @type {angular.IScope} */
+    #$scope;
+
+    /** @type {angular.IRootScopeService} */
+    #$rootScope;
+
+    /** @type {MongoDbService} */
+    #mongoDb;
+
+    static $inject = ['$scope', '$rootScope', 'mongoDb'];
+    constructor($scope, $rootScope, mongoDb) {
+        this.#$scope = $scope;
+        this.#$rootScope = $rootScope;
+        this.#mongoDb = mongoDb;
+        $scope.load = () => this.load();
+        $scope.upload = () => this.upload();
+        $scope.download = () => this.download();
+        $scope.export = () => this.export();
+        $scope.getImages = () => this.getImages();
+        $scope.removeIds = () => this.removeIds();
+        $scope.newData = {
+            filepath: path.join(os.homedir(), 'Pictures'),
+            recursive: true,
+        };
+        $scope.exportPath = path.join(process.cwd(), 'export.csv');
         $scope.data = null;
-        if ($scope.file1.length > 0) {
+        this.getMongoCollections();
+    }
+
+    load() {
+        this.#$scope.data = null;
+        if (this.#$scope.file1.length > 0) {
             try {
-                $scope.data = parseCsv(fs.readFileSync($scope.file1[0].path), {
+                this.#$scope.data = parseCsv(fs.readFileSync(this.#$scope.file1[0].path), {
                     columns: ['image', 'rating', 'class', 'isLabeled', '_id'],
                     cast: function (value, context) {
                         switch (context.column) {
@@ -28,43 +59,43 @@ function controllerFn($scope, $rootScope, mongoDb) {
             }
             catch (e) {
                 console.error(e);
-                $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: CSV', 'Loading or parsing error.');
+                this.#$rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: CSV', 'Loading or parsing error.');
             }
         }
-    };
+    }
 
-    $scope.upload = async function () {
+    async upload() {
         try {
-            $rootScope.$broadcast('LOADING_MODAL_SHOW', 'MongoDB', 'Uploading...');
-            await mongoDb.insertMany($scope.data, $scope.collectionPush);
-            await getMongoCollections();
-            $rootScope.$broadcast('LOADING_MODAL_HIDE');
+            this.#$rootScope.$broadcast('LOADING_MODAL_SHOW', 'MongoDB', 'Uploading...');
+            await this.#mongoDb.insertMany(this.#$scope.data, this.#$scope.collectionPush);
+            await this.getMongoCollections();
+            this.#$rootScope.$broadcast('LOADING_MODAL_HIDE');
         }
         catch (e) {
             console.error(e);
-            $rootScope.$broadcast('LOADING_MODAL_HIDE');
-            $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: MongoDB Query', 'Something happened while uploading to MongoDB.');
+            this.#$rootScope.$broadcast('LOADING_MODAL_HIDE');
+            this.#$rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: MongoDB Query', 'Something happened while uploading to MongoDB.');
         }
-        $scope.$apply();
-    };
+        this.#$scope.$apply();
+    }
 
-    $scope.download = async function () {
-        $scope.data = null;
+    async download() {
+        this.#$scope.data = null;
         try {
-            $rootScope.$broadcast('LOADING_MODAL_SHOW', 'MongoDB', 'Downloading...');
-            $scope.data = await mongoDb.getAll($scope.collectionPull);
-            $rootScope.$broadcast('LOADING_MODAL_HIDE');
+            this.#$rootScope.$broadcast('LOADING_MODAL_SHOW', 'MongoDB', 'Downloading...');
+            this.#$scope.data = await this.#mongoDb.getAll(this.#$scope.collectionPull);
+            this.#$rootScope.$broadcast('LOADING_MODAL_HIDE');
         }
         catch (e) {
             console.error(e);
-            $rootScope.$broadcast('LOADING_MODAL_HIDE');
-            $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: MongoDB Query', 'Something happened while downloading from MongoDB.');
+            this.#$rootScope.$broadcast('LOADING_MODAL_HIDE');
+            this.#$rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: MongoDB Query', 'Something happened while downloading from MongoDB.');
         }
-        $scope.$apply();
-    };
+        this.#$scope.$apply();
+    }
 
-    $scope.export = function () {
-        fs.writeFileSync($scope.exportPath, stringifyCsv($scope.data, {
+    export() {
+        fs.writeFileSync(this.#$scope.exportPath, stringifyCsv(this.#$scope.data, {
             columns: [
                 { key: 'image' },
                 { key: 'rating' },
@@ -73,46 +104,35 @@ function controllerFn($scope, $rootScope, mongoDb) {
                 { key: '_id' },
             ],
         }));
-    };
+    }
 
-    $scope.getImages = function () {
-        $scope.data = null;
-        $scope.data = fs.readdirSync($scope.newData.filepath, { withFileTypes: true }).
+    getImages() {
+        this.#$scope.data = null;
+        this.#$scope.data = fs.readdirSync(this.#$scope.newData.filepath, { withFileTypes: true }).
             filter(f => f.isFile() && ['.jpg', '.jpeg'].includes(path.extname(f.name).toLowerCase())).
             map(f => {
                 return {
-                    image: path.join($scope.newData.filepath, f.name),
+                    image: path.join(this.#$scope.newData.filepath, f.name),
                     isLabeled: false,
                 };
             });
-    };
-
-    $scope.removeIds = function () {
-        for (let i of $scope.data) {
-            delete i._id;
-        }
-    };
-
-    async function getMongoCollections() {
-        $scope.collections = await mongoDb.collections();
-        if ($scope.collections.length > 0) {
-            $scope.collectionPull = $scope.collections[0];
-        }
-        $scope.$apply();
     }
 
-    $scope.newData = {
-        filepath: path.join(os.homedir(), 'Pictures'),
-        recursive: true,
-    };
+    removeIds() {
+        for (let i of this.#$scope.data) {
+            delete i._id;
+        }
+    }
 
-    $scope.exportPath = path.join(process.cwd(), 'export.csv');
-    getMongoCollections();
-    $scope.data = null;
+    async getMongoCollections() {
+        this.#$scope.collections = await this.#mongoDb.collections();
+        if (this.#$scope.collections.length > 0) {
+            this.#$scope.collectionPull = this.#$scope.collections[0];
+        }
+        this.#$scope.$apply();
+    }
 
 }
 
-controllerFn.$inject = ['$scope', '$rootScope', 'mongoDb'];
 
-
-export default controllerFn;
+export default Controller;
