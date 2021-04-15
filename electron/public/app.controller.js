a83717d2add9ce9cde133e42f5f6a985309d8546
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-15 01:26:57 -0700

    Fix bug

--- electron/public/app.controller.js
+++ electron/public/app.controller.js
@@ -1,8 +1,21 @@
-function controllerFn($scope, screenView) {
-    $scope.screenView = screenView;
+const angular = require('angular');
+import { Screen, ScreenView } from './services/screen-view.service.js'
+
+class Controller {
+
+    static $inject = ['$scope', 'screenView'];
+
+    /**
+     * @param {angular.IScope} $scope
+     * @param {ScreenView} screenView
+     */
+    constructor($scope, screenView) {
+        $scope.screenView = screenView;
+        $scope.isMain = () => screenView.screen === Screen.MAIN;
+        $scope.isImage = () => screenView.screen === Screen.IMAGE;
+    }
+
 }
 
-controllerFn.$inject = ['$scope', 'screenView'];
 
-
-export default controllerFn;
+export default Controller;
