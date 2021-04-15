eba3f17ef9bbd173abec09207858c66bcbed7870
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-14 17:37:39 -0700

    Update options.service.js

--- electron/public/services/options.service.js
+++ electron/public/services/options.service.js
@@ -1,52 +1,57 @@
-function serviceFn($rootScope, $http) {
+const angular = require('angular');
 
-    class Options {
 
-        #isLoaded = false;
-        architectures = [];
-        datasets = [];
-        losses = [];
-        optimizers = [];
-        clusters = [];
+class Options {
 
-        constructor() { this.load(false); }
+    #isLoaded = false;
+    architectures = [];
+    datasets = [];
+    losses = [];
+    optimizers = [];
+    clusters = [];
 
-        async load(reload) {
-            if (!this.#isLoaded || reload) {
-                try {
-                    this.#isLoaded = false;
-                    $rootScope.$broadcast('LOADING_MODAL_SHOW', 'Deep Learning Options', 'Retrieving...');
-                    this.architectures = [];
-                    this.datasets = [];
-                    this.losses = [];
-                    this.optimizers = [];
-                    this.clusters = [];
-                    var response = await $http.get('http://localhost:8080/options');
-                    this.architectures = response.data.architectures;
-                    this.datasets = response.data.datasets;
-                    this.losses = response.data.losses;
-                    this.optimizers = response.data.optimizers;
-                    this.clusters = response.data.clusters;
-                    this.#isLoaded = true;
-                    $rootScope.$broadcast('LOADING_MODAL_HIDE');
-                }
-                catch (e) {
-                    console.error(e);
-                    $rootScope.$broadcast('LOADING_MODAL_HIDE');
-                    $rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: Deep Learning Options', '');
-                }
-                $rootScope.$apply();
-            }
-        }
+    /** @type angular.IRootScopeService */
+    #$rootScope;
 
+    /** @type angular.IHttpService */
+    #$http;
+
+    static $inject = ['$rootScope', '$http'];
+    constructor($rootScope, $http) {
+        this.#$rootScope = $rootScope;
+        this.#$http = $http;
+        this.load(false);
     }
 
-    return new Options();
+    async load(reload) {
+        if (!this.#isLoaded || reload) {
+            try {
+                this.#isLoaded = false;
+                this.#$rootScope.$broadcast('LOADING_MODAL_SHOW', 'Deep Learning Options', 'Retrieving...');
+                this.architectures = [];
+                this.datasets = [];
+                this.losses = [];
+                this.optimizers = [];
+                this.clusters = [];
+                var response = await this.#$http.get('http://localhost:8080/options');
+                this.architectures = response.data.architectures;
+                this.datasets = response.data.datasets;
+                this.losses = response.data.losses;
+                this.optimizers = response.data.optimizers;
+                this.clusters = response.data.clusters;
+                this.#isLoaded = true;
+                this.#$rootScope.$broadcast('LOADING_MODAL_HIDE');
+            }
+            catch (e) {
+                console.error(e);
+                this.#$rootScope.$broadcast('LOADING_MODAL_HIDE');
+                this.#$rootScope.$broadcast('ERROR_MODAL_SHOW', e, 'Error: Deep Learning Options', '');
+            }
+            this.#$rootScope.$apply();
+        }
+    }
 
 }
 
 
-serviceFn.$inject = ['$rootScope', '$http'];
-
-
-export default serviceFn;
+export default Options;
