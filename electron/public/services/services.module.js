2117c6d403e2d6075d6dfd55adb9fbfe0d3a6ee9
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-15 22:45:17 -0700

    Update services.module.js

--- electron/public/services/services.module.js
+++ electron/public/services/services.module.js
@@ -1,23 +1,23 @@
 const angular = require('angular');
-import history from './history.service.js';
-import cwd from './cwd.service.js';
-import screenView from './screen-view.service.js';
+import History from './history.service.js';
+import Cwd from './cwd.service.js';
+import ScreenView from './screen-view.service.js';
 import queryServer from './query-server.service.js';
-import options from './options.service.js';
-import mongoDb from './mongodb.service.js';
-import focusImage from './focus-image.service.js';
-import mongoDbSettings from './mongodb-settings.service.js';
+import Options from './options.service.js';
+import MongoDb from './mongodb.service.js';
+import FocusImage from './focus-image.service.js';
+import MongoDbSettings from './mongodb-settings.service.js';
 
 
 const module = angular.module('services', []);
-module.factory('history', history);
-module.factory('cwd', cwd);
-module.factory('screenView', screenView);
+module.factory('history', History);
+module.factory('cwd', Cwd);
+module.factory('screenView', ScreenView);
 module.factory('queryServer', queryServer);
-module.factory('options', options);
-module.factory('mongoDb', mongoDb);
-module.factory('focusImage', focusImage);
-module.factory('mongoDbSettings', mongoDbSettings);
+module.factory('options', Options);
+module.factory('mongoDb', MongoDb);
+module.factory('focusImage', FocusImage);
+module.factory('mongoDbSettings', MongoDbSettings);
 
 
 export default module.name;
