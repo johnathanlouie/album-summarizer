af26a039342794ec88546a89f51412ff0a2dcd32
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-17 20:35:47 -0700

    Implement training status/evaluation viewer

--- electron/public/views/status-view/status-view.controller.js
+++ electron/public/views/status-view/status-view.controller.js
@@ -2,6 +2,7 @@ const angular = require('angular');
 import QueryServerService from '../../services/query-server.service.js';
 import ModalService from '../../services/modal.service.js';
 import OptionsService from '../../services/options.service.js';
+import MongoDbService from '../../services/mongodb.service.js';
 
 
 class Controller {
@@ -10,39 +11,52 @@ class Controller {
     #queryServer;
     #modal;
 
-    static $inject = ['$scope', 'queryServer', 'modal', 'options'];
+    static $inject = ['$scope', 'queryServer', 'modal', 'options', 'mongoDb'];
 
     /**
      * @param {angular.IScope} $scope 
      * @param {QueryServerService} queryServer
      * @param {ModalService} modal
      * @param {OptionsService} options
+     * @param {MongoDbService} mongoDb
      */
-    constructor($scope, queryServer, modal, options) {
+    constructor($scope, queryServer, modal, options, mongoDb) {
         this.#scope = $scope;
         this.#queryServer = queryServer;
         this.#modal = modal;
 
+        $scope.keepGoing = true;
+
         async function loadOptions() {
-            try {
-                modal.showLoading('RETRIEVING...');
-                await options.load();
-                modal.hideLoading();
-            }
-            catch (e) {
-                console.error(e);
-                modal.hideLoading();
-                modal.showError(e, 'ERROR: Deep Learning', 'Error during fetching compile options');
-            }
+            await options.load();
         }
 
-        $scope.evaluations = [];
+        async function getEvaluated() {
+            $scope.evaluations = await mongoDb.getAll('evaluations');
+        }
+
+        function isEqual(a, b) {
+            for (let i in a) {
+                if (a[i] !== b[i]) { return false; }
+            }
+            return true;
+        }
+
+        function isEvaluated(model) {
+            for (let i of $scope.evaluations) {
+                if (isEqual(i.model, model)) { return true; }
+            }
+            return false;
+        }
 
         async function doAll() {
             for (let a of options.architectures) {
                 for (let d of options.datasets) {
                     for (let l of options.losses) {
                         for (let o of options.optimizers) {
+                            if (!$scope.keepGoing) {
+                                return;
+                            }
                             let model = {
                                 architecture: a,
                                 dataset: d,
@@ -53,16 +67,36 @@ class Controller {
                                 patience: 3,
                                 split: 0,
                             };
-                            let result = await queryServer.evaluate(model);
-                            $scope.evaluations.push(result);
-                            $scope.$apply();
+                            if (!isEvaluated(model)) {
+                                $scope.isRunning = true;
+                                $scope.$apply();
+                                let result = await queryServer.evaluate(model);
+                                await mongoDb.insertOne('evaluations', result);
+                                $scope.evaluations.push(result);
+                                $scope.isRunning = false;
+                                $scope.$apply();
+                            }
                         }
                     }
                 }
             }
         }
 
-        loadOptions().then(() => doAll());
+        async function init() {
+            console.log('load screen');
+            modal.showLoading('RETRIEVING...');
+            try {
+                await Promise.all([getEvaluated(), loadOptions()]);
+                modal.hideLoading();
+                await doAll();
+            } catch (e) {
+                console.error(e);
+                modal.hideLoading();
+                modal.showError(e, 'ERROR: Deep Learning', 'Error while evaluating');
+            }
+        }
+
+        init();
     }
 
     async evaluateAll() {
