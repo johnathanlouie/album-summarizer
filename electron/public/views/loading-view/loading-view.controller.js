04d7da94e73a1e610ae7921141bf3e89ee951e97
Johnathan Louie <johnathanlouie@outlook.com>
2021-04-29 17:33:34 -0700

    Fix imports

--- electron/public/views/loading-view/loading-view.controller.js
+++ electron/public/views/loading-view/loading-view.controller.js
@@ -1,47 +1,36 @@
 const angular = require('angular');
-const _ = require('lodash');
-const mongodb = require('mongodb');
-import QueryServerService from '../../services/query-server.service.js';
 import ModalService from '../../services/modal.service.js';
 import OptionsService from '../../services/options.service.js';
-import MongoDbService from '../../services/mongodb.service.js';
 import UsersService from '../../services/users.service.js';
 import EvaluationsService from '../../services/evaluations.service.js';
 
 
-class Controller {
+class LoadingViewController {
 
-    #scope;
-    #queryServer;
-    #modal;
-    #options;
-    #mongoDb;
-    #location;
-    #users;
-    #evaluations;
+    $scope;
+    $location;
+    modal;
+    options;
+    users;
+    evaluations;
 
-    static $inject = ['$scope', '$location', 'queryServer', 'modal', 'options', 'mongoDb', 'users', 'evaluations'];
+    static $inject = ['$scope', '$location', 'modal', 'options', 'users', 'evaluations'];
 
     /**
      * @param {angular.IScope} $scope 
      * @param {angular.ILocationService} $location
-     * @param {QueryServerService} queryServer
      * @param {ModalService} modal
      * @param {OptionsService} options
-     * @param {MongoDbService} mongoDb
-     * @param {MongoDbService} mongoDb
      * @param {UsersService} users
      * @param {EvaluationsService} evaluations
      */
-    constructor($scope, $location, queryServer, modal, options, mongoDb, users, evaluations) {
-        this.#scope = $scope;
-        this.#location = $location;
-        this.#queryServer = queryServer;
-        this.#modal = modal;
-        this.#options = options;
-        this.#mongoDb = mongoDb;
-        this.#users = users;
-        this.#evaluations = evaluations;
+    constructor($scope, $location, modal, options, users, evaluations) {
+        this.$scope = $scope;
+        this.$location = $location;
+        this.modal = modal;
+        this.options = options;
+        this.users = users;
+        this.evaluations = evaluations;
 
         $scope.status = 'CONNECTING';
         $scope.connectionFailed = false;
@@ -54,25 +43,25 @@ class Controller {
 
     async loadAll() {
         try {
-            this.#scope.status = 'CONNECTING';
-            this.#scope.connectionFailed = false;
+            this.$scope.status = 'CONNECTING';
+            this.$scope.connectionFailed = false;
             await Promise.all([
-                this.#options.load(),
-                this.#users.load(),
-                this.#evaluations.fetchStatuses(),
+                this.options.load(),
+                this.users.load(),
+                this.evaluations.fetchStatuses(),
             ]);
-            this.#location.path('/organizer');
+            this.$location.path('/organizer');
         }
         catch (e) {
             console.error(e);
-            this.#scope.status = 'DISCONNECTED';
-            this.#scope.connectionFailed = true;
-            this.#modal.showError(e, 'ERROR: Server/MongoDB Connection', 'Errors during loading of program.');
+            this.$scope.status = 'DISCONNECTED';
+            this.$scope.connectionFailed = true;
+            this.modal.showError(e, 'ERROR: Server/MongoDB Connection', 'Errors during loading of program.');
         }
-        this.#scope.$apply();
+        this.$scope.$apply();
     }
 
 }
 
 
-export default Controller;
+export default LoadingViewController;
