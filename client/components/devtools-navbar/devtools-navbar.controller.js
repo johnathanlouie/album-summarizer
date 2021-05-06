854a7aafb7a3f2337208f1fef1f92a702b2a57b3
Johnathan Louie <johnathanlouie@outlook.com>
2021-05-06 14:55:00 -0700

    Change the loading page button into a reload button

--- client/components/devtools-navbar/devtools-navbar.controller.js
+++ client/components/devtools-navbar/devtools-navbar.controller.js
@@ -1,12 +1,15 @@
 import RouteManager from '../../lib/routes.js';
 
 
-function controllerFn($scope, $location) {
+function controllerFn($scope, $location, $window) {
     $scope.routes = function () { return RouteManager.menu2() };
     $scope.isActive = function (route) { return $location.path() === route; };
+    $scope.reload = function () {
+        $window.location = `index.html?reload=${(new Date()).getTime()}`;
+    };
 }
 
-controllerFn.$inject = ['$scope', '$location'];
+controllerFn.$inject = ['$scope', '$location', '$window'];
 
 
 export default controllerFn;
