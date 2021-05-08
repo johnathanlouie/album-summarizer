468550c4e239c29f74cf84e1a12689dcbb7a78ce
Johnathan Louie <johnathanlouie@outlook.com>
2021-05-07 22:02:07 -0700

    Add more specific loading screen

--- client/views/loading-view/loading-view.controller.js
+++ client/views/loading-view/loading-view.controller.js
@@ -5,7 +5,36 @@ import UsersService from '../../services/users.service.js';
 import EvaluationsService from '../../services/evaluations.service.js';
 
 
+class Statuses {
+
+    options = false;
+    users = false;
+    evaluations = false;
+    evaluationStatuses = false;
+    total = 4;
+
+    reset() {
+        this.options = false;
+        this.users = false;
+        this.evaluations = false;
+        this.evaluationStatuses = false;
+    }
+
+    get succeeded() {
+        let n = 0;
+        if (this.options) { n++; }
+        if (this.users) { n++; }
+        if (this.evaluations) { n++; }
+        if (this.evaluationStatuses) { n++; }
+        return n;
+    }
+
+}
+
+
 class LoadingViewController {
+
+    #statuses = new Statuses();
 
     static $inject = ['$scope', '$location', '$q', 'modal', 'options', 'users', 'evaluations'];
     $scope;
@@ -36,6 +65,7 @@ class LoadingViewController {
 
         $scope.status = 'CONNECTING';
         $scope.connectionFailed = false;
+        $scope.statuses = this.#statuses;
         $scope.retry = () => this.loadAll();
 
         this.loadAll();
@@ -46,13 +76,21 @@ class LoadingViewController {
     loadAll() {
         this.$scope.status = 'CONNECTING';
         this.$scope.connectionFailed = false;
-        return this.$q.all([
-            this.options.load(),
-            this.users.load(),
-            this.evaluations.fetchStatuses(),
-            this.evaluations.fromMongoDb(),
-        ]).then(
-            () => this.$location.path('/organizer')
+        this.#statuses.reset();
+        return this.$q.resolve(Promise.allSettled([
+            this.$q.resolve(this.options.load()).then(() => { this.#statuses.options = true; }),
+            this.$q.resolve(this.users.load()).then(() => { this.#statuses.users = true; }),
+            this.$q.resolve(this.evaluations.fromMongoDb()).then(() => { this.#statuses.evaluations = true; }),
+            this.$q.resolve(this.evaluations.fetchStatuses()).then(() => { this.#statuses.evaluationStatuses = true; }),
+        ])).then(
+            loadResults => {
+                for (let i of loadResults) {
+                    if (i.status === 'rejected') {
+                        throw new Error();
+                    }
+                }
+                this.$location.path('/organizer');
+            }
         ).catch(
             e => {
                 console.error(e);
