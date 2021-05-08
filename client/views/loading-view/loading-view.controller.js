682c636d0d13b3c8d3955b994fc40df37a88fb49
Johnathan Louie <johnathanlouie@outlook.com>
2021-05-08 01:49:24 -0700

    Center loading statuses; send array of errors to error modal

--- client/views/loading-view/loading-view.controller.js
+++ client/views/loading-view/loading-view.controller.js
@@ -5,27 +5,32 @@ import UsersService from '../../services/users.service.js';
 import EvaluationsService from '../../services/evaluations.service.js';
 
 
+const OK = 'OK';
+const LOADING = 'LOADING...';
+const FAIL = 'FAILED';
+
+
 class Statuses {
 
-    options = false;
-    users = false;
-    evaluations = false;
-    evaluationStatuses = false;
+    options = LOADING;
+    users = LOADING;
+    evaluations = LOADING;
+    evaluationStatuses = LOADING;
     total = 4;
 
     reset() {
-        this.options = false;
-        this.users = false;
-        this.evaluations = false;
-        this.evaluationStatuses = false;
+        this.options = LOADING;
+        this.users = LOADING;
+        this.evaluations = LOADING;
+        this.evaluationStatuses = LOADING;
     }
 
     get succeeded() {
         let n = 0;
-        if (this.options) { n++; }
-        if (this.users) { n++; }
-        if (this.evaluations) { n++; }
-        if (this.evaluationStatuses) { n++; }
+        if (this.options === OK) { n++; }
+        if (this.users === OK) { n++; }
+        if (this.evaluations === OK) { n++; }
+        if (this.evaluationStatuses === OK) { n++; }
         return n;
     }
 
@@ -78,24 +83,30 @@ class LoadingViewController {
         this.$scope.connectionFailed = false;
         this.#statuses.reset();
         return this.$q.resolve(Promise.allSettled([
-            this.$q.resolve(this.options.load()).then(() => { this.#statuses.options = true; }),
-            this.$q.resolve(this.users.load()).then(() => { this.#statuses.users = true; }),
-            this.$q.resolve(this.evaluations.fromMongoDb()).then(() => { this.#statuses.evaluations = true; }),
-            this.$q.resolve(this.evaluations.fetchStatuses()).then(() => { this.#statuses.evaluationStatuses = true; }),
+            this.$q.resolve(this.options.load()).then(
+                () => { this.#statuses.options = OK; },
+                e => { this.#statuses.options = FAIL; throw e; },
+            ),
+            this.$q.resolve(this.users.load()).then(
+                () => { this.#statuses.users = OK; },
+                e => { this.#statuses.users = FAIL; throw e; },
+            ),
+            this.$q.resolve(this.evaluations.fromMongoDb()).then(
+                () => { this.#statuses.evaluations = OK; },
+                e => { this.#statuses.evaluations = FAIL; throw e; },
+            ),
+            this.$q.resolve(this.evaluations.fetchStatuses()).then(
+                () => { this.#statuses.evaluationStatuses = OK; },
+                e => { this.#statuses.evaluationStatuses = FAIL; throw e; },
+            ),
         ])).then(
             loadResults => {
-                let hasErrors = false;
-                for (let i of loadResults) {
-                    if (i.status === 'rejected') {
-                        hasErrors = true;
-                        break;
-                    }
-                }
-                if (hasErrors) {
+                let errors = loadResults.filter(i => i.status === 'rejected').map(i => i.reason);
+                if (errors.length > 0) {
                     console.table(loadResults);
                     this.$scope.status = 'DISCONNECTED';
                     this.$scope.connectionFailed = true;
-                    this.modal.showError(e, 'ERROR: Server/MongoDB Connection', 'Errors during loading of program.');
+                    this.modal.showError(errors, 'ERROR: Server/MongoDB Connection', 'Errors during loading of program.');
                 }
                 else {
                     this.$location.path('/organizer');
