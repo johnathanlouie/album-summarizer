a590e6078ddb135de36fc63477aa9796ecf9e875
Johnathan Louie <johnathanlouie@outlook.com>
2021-05-10 03:18:01 -0700

    Update database.service.js

--- client/services/database.service.js
+++ client/services/database.service.js
@@ -1,5 +1,26 @@
+const angular = require('angular');
 import MongoDbService from './mongodb.service.js';
 import { Evaluation } from '../lib/evaluation.js';
+
+
+class RatingClass {
+
+    /** @type {mongodb.ObjectID} */
+    _id;
+
+    /** @type {string} */
+    image;
+
+    /** @type {number} */
+    rating;
+
+    /** @type {string} */
+    class;
+
+    /** @type {boolean} */
+    isLabeled;
+
+}
 
 
 class DatabaseService {
@@ -43,6 +64,15 @@ class DatabaseService {
      */
     deleteEvaluation(evaluation) {
         return this.mongoDb.findOneAndDelete('evaluations', { _id: evaluation._id });
+    }
+
+    /**
+     * 
+     * @param {string} username 
+     * @returns {angular.IPromise.<RatingClass>}
+     */
+    getRatingClass(username) {
+        return this.mongoDb.getAll(username);
     }
 
 }
