b200080fe09673914c0e762ed3324be7cb75dd1e
Johnathan Louie <johnathanlouie@outlook.com>
2021-05-25 17:40:51 -0700

    Add resnet101a

--- python/architecture/resnet.py
+++ python/architecture/resnet.py
@@ -6,6 +6,8 @@ from core.jl import Resolution
 from core.modeltype import OutputType
 from keras.applications.resnet import ResNet101
 from keras.engine.input_layer import Input
+from keras.layers import (BatchNormalization, Conv2D, Dense, Flatten,
+                          MaxPooling2D)
 from keras.models import Model
 
 
@@ -31,4 +33,29 @@ class Resnet101(Architecture):
         )
 
 
+class Resnet101a(Architecture):
+    """
+    Untrained VGG16 for classification.
+    """
+
+    NAME = 'resnet101a'
+    OUTPUT_TYPE: OutputType = OutputType.SCALAR
+
+    def create(self, res: Resolution, classes: Optional[int]) -> Model:
+        """
+        Creates a keras.models.Model object.
+        """
+        img_input = Input(res.hwc())
+        x = ResNet101(
+            include_top=True,
+            weights=None,
+            input_tensor=img_input,
+            classes=classes,
+        )
+        x = Dense(1, activation='relu', name='predictions')(x.output)
+        model = Model(img_input, x, name='resnet101a')
+        return model
+
+
 modelbuilder.ModelBuilder.architecture(Resnet101())
+modelbuilder.ModelBuilder.architecture(Resnet101a())
