4120b2c542a0f0be6d13ab4decfe5220996cd1b6
Johnathan Louie <johnathanlouie@outlook.com>
2021-05-27 19:20:14 -0700

    Combine other command line scripts into one

---
+++ python/cli.py
@@ -0,0 +1,138 @@
+import argparse
+import json
+from typing import Dict
+
+import aaa
+import addon
+import core.modelbuilder
+import core.model
+
+
+def doSomething(
+        architecture,
+        dataset,
+        loss,
+        optimizer,
+        metrics,
+        epochs,
+        patience,
+        split=None,
+        summary=False,
+        train=False,
+        evaluate=False,
+        remove_bad=False
+):
+    try:
+        print()
+        print()
+        model = core.modelbuilder.ModelBuilder.create(
+            architecture,
+            dataset,
+            loss,
+            optimizer,
+            metrics,
+            epochs,
+            patience,
+        )
+        if summary:
+            model.summary()
+        else:
+            if split != None:
+                model = model.split(split)
+            if train:
+                if model.is_complete():
+                    print('Complete model: %s %s %s %s %s' % (
+                        architecture,
+                        dataset,
+                        loss,
+                        optimizer,
+                        'acc',
+                    ))
+                elif model.has_error():
+                    print('Error model: %s %s %s %s %s' % (
+                        architecture,
+                        dataset,
+                        loss,
+                        optimizer,
+                        'acc',
+                    ))
+                else:
+                    model.train()
+            if evaluate:
+                if model.is_complete():
+                    model.evaluate_training_set()
+                    model.evaluate_validation_set()
+                    model.evaluate_test_set()
+            if remove_bad:
+                if model.has_error():
+                    model.delete(True)
+    except core.model.BadModelSettings:
+        print("IGNORE: %s %s %s %s" % (architecture, dataset, loss, optimizer))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model')
+    parser.add_argument('--all', action='store_true')
+    parser.add_argument('--options', action='store_true')
+    parser.add_argument('--summary', action='store_true')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--evaluate', action='store_true')
+    parser.add_argument('--removebad', action='store_true')
+    args = parser.parse_args()
+    if args.options:
+        print()
+        print('Architectures')
+        for i in core.modelbuilder.ModelBuilder.ARCHITECTURES.keys():
+            print(' - %s' % i)
+        print('Data Sets')
+        for i in core.modelbuilder.ModelBuilder.DATASETS.keys():
+            print(' - %s' % i)
+        print('Losses')
+        for i in core.modelbuilder.ModelBuilder.LOSSES.keys():
+            print(' - %s' % i)
+        print('Optimizers')
+        for i in core.modelbuilder.ModelBuilder.OPTIMIZERS.keys():
+            print(' - %s' % i)
+        print('Metrics')
+        for i in core.modelbuilder.ModelBuilder.METRICS.keys():
+            print(' - %s' % i)
+        print()
+    elif args.model != None:
+        with open(args.model) as f:
+            settings: Dict[str, str] = json.load(f)
+            doSomething(
+                settings['architecture'],
+                settings['dataset'],
+                settings['loss'],
+                settings['optimizer'],
+                settings['metrics'],
+                settings['epochs'],
+                settings['patience'],
+                settings.get('split'),
+                args.summary,
+                args.train,
+                args.evaluate,
+                args.removebad,
+            )
+    elif args.all:
+        for architecture, dataset, loss, optimizer in core.modelbuilder.ModelBuilder.builds():
+            print()
+            doSomething(
+                architecture,
+                dataset,
+                loss,
+                optimizer,
+                'acc',
+                epochs=0,
+                patience=3,
+                split=0,
+                summary=args.summary,
+                train=args.train,
+                evaluate=args.evaluate,
+                remove_bad=args.removebad,
+            )
+
+
+if __name__ == '__main__':
+    main()
