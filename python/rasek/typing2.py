de215a72f1d8969e2f526d79b539e603784e1751
Johnathan Louie <johnathanlouie@outlook.com>
2021-06-24 20:24:23 -0700

    Create typing2.py

---
+++ python/rasek/typing2.py
@@ -0,0 +1,14 @@
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from numpy import ndarray
+
+
+ArrayLike = Union[ndarray, List[Any]]
+Url = str
+Image = ndarray
+number = Union[int, float]
+OneHot = ndarray
+IntClass = int
+StrClass = str
+ImageArray = ndarray
+Primitive = Union[int, float, str, bool]
